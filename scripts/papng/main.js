70b09e6f31923e703c821d9f0cb12bb70de1807c
Caleen Boyter <caleenboyter@gmail.com>
2023-09-13 14:05:26 -0400

    added import, export, and modularizing

---
+++ scripts/papng/main.js
@@ -0,0 +1,13 @@
+import { getCity } from './cities/citiesDB.js'
+
+const allCities = getCity()
+
+for (const singleCity of allCities) {
+    console.log(singleCity)
+}
+
+import {CityList} from `./CityList.js`
+
+const parentHTMLElement = document.querySelector(".city")
+
+parentHTMLElement.innerHTML = CityList()
