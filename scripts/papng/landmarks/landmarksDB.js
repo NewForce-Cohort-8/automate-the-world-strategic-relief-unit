699fc1750f77f8e08dfe0aa62af864ecc0eefa4d
Caleen Boyter <caleenboyter@gmail.com>
2023-09-13 09:43:56 -0400

    added copy and images to db

--- scripts/papng/landmarks/landmarksDB.js
+++ scripts/papng/landmarks/landmarksDB.js
@@ -4,18 +4,18 @@ const database = {
            
         {
             name: "Port Moresby Nature Park",
-            features: "",
-            image: ""
+            features: "Spread over 30 acres and located next to the University of Papua New Guinea, the park is home to over 550 native animals and hundreds of plant species, many of which are rarely seen outside of PNG.",
+            image: "https://bucketpng.s3.amazonaws.com/files/s3fs-public/2018-11/portmoresbynaturepark_1.png"
         },
         {
             name: "Minyé Sinkhole and Cave",
-            features: "",            
-            image: ""
+            features: "One of the largest and most impressive giant sinkholes in the world, located in an exotic jungle and crossed by a powerful underground river. Contains one of the largest cave rooms in the world",            
+            image: "https://www.wondermondo.com/wp-content/uploads/2017/10/Manjanggul.jpg"
         },
         {
             name: "Mageni Cave and Waterfall",
-            features: "",           
-            image: ""
+            features: "An enormous cave opening in the gorge with an 80 m tall waterfall falling into it. The beautiful cave has been mapped in a 7.2 km length.",           
+            image: "https://static.nationalgeographic.co.uk/files/styles/image_3200/public/51-iconic-waterfall-mageni-cave-new-britain-island-papua-new-guinea.webp?w=1190&h=794"
         },
     ]
 }
