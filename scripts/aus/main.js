9caf386e474957cab7fdc7f1b97f9aa7a31ded3d
Andrew Hanks <andymchanks@gmail.com>
2023-09-13 14:13:45 -0400

    new main

--- scripts/aus/main.js
+++ scripts/aus/main.js
@@ -1,18 +1,23 @@
-import { getFish } from './database.js'
-const allFish = getFish()
-for (const singleFish of allFish) {
-    console.log(singleFish)
+import { getPeople } from './people/peopleDB.js'
+
+const allPeople = getPeople()
+
+for (const singlePeople of allPeople) {
+    console.log(singlePeople)
 }
+
+
 // Import the FishList function from the correct module
-import { FishList } from './FishList.js'
+import { PeopleList } from './PeopleList.js'
 
 /*
     What is the CSS selector for the element where you
     want to display the fish?
+
     Use . for elements with a "class" attribute
     Use # for elements with an "id" attribute
  */
-const newSectionForFishList = document.querySelector(".card_parent")
+const parentHTMLElement = document.querySelector(".people")
 
 
-newSectionForFishList.innerHTML = FishList()
+parentHTMLElement.innerHTML = PeopleList()
