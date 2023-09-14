c75097632b7759d4e8b5d9784fa7485f86a41a70
Andrew Hanks <andymchanks@gmail.com>
2023-09-14 10:23:26 -0400

    Functions work

--- scripts/aus/cities/citiesDB.js
+++ scripts/aus/cities/citiesDB.js
@@ -6,12 +6,15 @@ const database = {
             {
                 cityName:"BARTERTOWN",
                 cityInfo:"Bartertown is a remote market-town outpost situated in the midst of the Wasteland. Its primary focus and income is the trade and bartering with scavengers in the area, and travelers passing through.",
-                cityImage:"https://static.wikia.nocookie.net/roadwarrior/images/8/8e/Thunderdome.png/revision/latest?cb=20150716040731"
-            },            {
+                cityImage:"https://www.alamy.com/tina-turner-mad-max-beyond-thunderdome-1985-image475562722.html",
+
+            },
+            {
                 cityName:"SYDNEY",
                 cityInfo:"Sydney is the capital city of the state of New South Wales, and the most populous city in Australia",
                 cityImage:"https://ontheworldmap.com/australia/city/sydney/sydney-location-on-the-australia-map.jpg"
-            },            {
+            },            
+            {
                 cityName:"MELBOURNE",
                 cityInfo:"Melbourne is in the southeastern part of mainland Australia, within the state of Victoria. Geologically, it is built on the confluence of Quaternary lava flows",
                 cityImage:"https://www.celebritycruises.com/blog/content/uploads/2022/12/best-beaches-in-melbourne-australia-brighton-beach-hero.jpg"
